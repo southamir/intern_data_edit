1ba03368a1e78ec025771bbb263abcf21f62bb7d
southamir <t_m_x12x@yahoo.co.jp>
2017-02-20 17:50:03 +0900

    commit on 20th Feb

--- data_edit.py
+++ data_edit.py
@@ -2,9 +2,8 @@ import pandas as pd
 
 # Read csv file
 df = pd.read_csv("../intern/training_data.csv")
+# df = pd.read_csv("../intern/evaluation_data.csv")
 
-
-print(df.dtypes)
 
 # Delete months
 df['term'] = df['term'].str.extract('([0-9]+)').astype(int)
@@ -12,7 +11,6 @@ df['term'] = df['term'].str.extract('([0-9]+)').astype(int)
 # Delete %
 df['int_rate'] = df['int_rate'].str.extract('([0.0-9]+)').astype(float)
 df['revol_util'] = df['revol_util'].str.extract('([0.0-9]+)').astype(float)
-
 
 # Replace data
 df['emp_length'] = df['emp_length'].mask(df['emp_length'] == '10+ years', '10')
@@ -23,15 +21,15 @@ df['emp_length'] = df['emp_length'].str.extract('([0-9]+)').astype(int)
 # Quantification grade
 grades = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 df['grade'] = df['grade'].map(lambda x: grades.index(x))
-'''
-#for i in range(len(grades)):
-df['sub_grade'] = df['sub_grade'].str.extract('([A-G])')
-print(df['sub_grade'])
-# .mask(df['sub_grade'] == grades[i], str(i) + '.')
-'''
+for i in range(len(grades)):
+    for j in range(5):
+        df['sub_grade'] = df['sub_grade'].mask(df['sub_grade'] == (grades[i] + str(j + 1)), (str(i) + '.' + str(j)))
+df['sub_grade'] = df['sub_grade'].str.extract('([0.0-9]+)').astype(float)
 
-print(df.dtypes)
-
+# if ... del elements
+'''
+del df['sub_grade']
+'''
 
 # Output csv
 df.to_csv("output.csv", index=None)
